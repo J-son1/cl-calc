7ffbb5aa495f8ddac93149f3a70b68bc9f44b5ac
Jason Humbert <jasonhumbert@outlook.com>
2022-05-25 20:41:29 +0100

    Add isOperator function

--- index.ts
+++ index.ts
@@ -5,7 +5,20 @@ function main(): void {
   const operator: string = question('Enter operator:\n');
   const secondStr: string = question('Enter second number\n');
 
-  const firstNum = isNumber(firstStr);
+  const op = isOperator(operator);
+  console.log(op);
+}
+
+function isOperator(operator: string): boolean {
+  switch(operator) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+      return true;
+    default:
+      return false;
+  }
 }
 
 function isNumber(str: string): boolean {
